bd826da73a58247cbd98d14f20361f1a5b92d133
Mary Paul <marymc135@hotmail.com>
2022-11-10 20:43:57 -0500

    back end files

---
+++ server/models/Workout.js
@@ -0,0 +1,23 @@
+const { Schema, model, SchemaTypes } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const workoutSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true, 
+            unique: false
+        },
+        userId: {
+            type: Number,
+            required: true,
+            unique: false
+        },
+        exercises:[]
+    }
+
+)
+
+const Workout = model('Workout', workoutSchema);
+
+module.exports = Workout;

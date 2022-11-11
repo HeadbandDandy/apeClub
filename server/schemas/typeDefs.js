60c3486c64b3aaa319db6d41ec5bb38baa898a81
mjd10m <mjd10m@outlook.com>
2022-11-10 21:07:44 -0500

    merge to develop

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,30 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    workouts: [Workout]
+  }
+
+  type Workout {
+    _id: ID
+    name: String
+    userId: String
+    exercises: []
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+  }
+`;
+
+module.exports = typeDefs;

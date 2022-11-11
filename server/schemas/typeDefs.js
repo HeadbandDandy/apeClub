7b937c9358887b85a7abf18884a34376c889809b
Mary Paul <marymc135@hotmail.com>
2022-11-10 20:50:12 -0500

    deftypes

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,1 +1,26 @@
+const { gql } = require('apollo-server-express');
 
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    workouts: [Workout]
+  }
+  type Workout {
+    _id: ID
+    name: String
+    userId: String
+    exercises: []
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+  type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+  }
+`;
+module.exports = typeDefs;

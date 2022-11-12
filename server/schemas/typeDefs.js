316d252870ec558285c1d65c14829dc3a52b56c6
Mary Paul <marymc135@hotmail.com>
2022-11-11 20:29:03 -0500

    typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -22,6 +22,14 @@ const typeDefs = gql`
     me: User
     users: [User]
     user(username: String!): User
+    workouts: [Workout]
+    workoutsByUser(user_id: Int): [Workout] 
+    workout(_id: ID!): Workout  
+  }
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    addWorkout(name: String!, exercises: [Int]): Workout
   }
 `;
 module.exports = typeDefs;
